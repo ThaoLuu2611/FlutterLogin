dcf888c721b8366519fabb18d9ca6813312002ae
thaoltp11 <thaoltp11@fsoft.com.vn>
2020-03-07 21:20:04 +0700

    show password

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -28,14 +20,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,31 +28,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  bool _showPass = false;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
         ),
         body: Container(
@@ -108,16 +73,20 @@ class _MyHomePageState extends State<MyHomePage> {
                 alignment: AlignmentDirectional.centerEnd,
                 children: <Widget>[
                   TextField(
-                    obscureText: true,
+                    obscureText: _showPass,
                     decoration: InputDecoration(
                         labelText: 'PASSWORD',
                         labelStyle: TextStyle(
                           fontSize: 15,
                         )),
                   ),
-                  Text(
-                    'Show',
-                    style: TextStyle(color: Colors.blue, fontSize: 14),
+                  GestureDetector(
+
+                    onTap: onShowClicked,
+                    child: Text(
+                      'Show',
+                      style: TextStyle(color: Colors.blue, fontSize: 14),
+                    ),
                   ),
                 ],
               ),
@@ -147,5 +116,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void onLogin(){
 
+  }
+
+  void onShowClicked(){
+    setState(() {
+      _showPass = !_showPass;
+    });
   }
 }
